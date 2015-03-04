96cd60827f78ad12fe80108477beea838ac793e6
malzev <marcos.al.azevedo@gmail.com>
2015-03-04 16:12:58 +0800

    Update and rename strlong to strlong/Test.java

---
+++ strlong/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+   public static void main(String[] args) {
+      String numberAsString = "1234";
+      long number = Long.parseLong(numberAsString);
+      System.out.println("The number is: " + number);
+   }
+}
