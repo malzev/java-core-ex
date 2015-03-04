c7c1ecbc6d62007e399b9d859ce2eb51d07d14cf
malzev <marcos.al.azevedo@gmail.com>
2015-03-04 16:16:00 +0800

    Update and rename strdbl to strdbl/Test.java

---
+++ strdbl/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+   public static void main(String[] args) {
+      String numberAsString = "153.25";
+      double number = Double.parseDouble(numberAsString);
+      System.out.println("The number is: " + number);
+   }
+}
