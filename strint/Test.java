f89328d7ad36732efe1c567c8520d85714a004bf
malzev <marcos.al.azevedo@gmail.com>
2015-03-04 16:11:28 +0800

    Update and rename strint to strint/Test.java

---
+++ strint/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+   public static void main(String[] args) {
+      String numberAsString = "1234";
+      int number = Integer.parseInt(numberAsString);
+      System.out.println("The number is: " + number);
+   }
+}
