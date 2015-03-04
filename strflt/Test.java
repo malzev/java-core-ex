bff539dcdec357b20787d066d059a97cca1e35ff
malzev <marcos.al.azevedo@gmail.com>
2015-03-04 16:13:50 +0800

    Update and rename strflt to strflt/Test.java

---
+++ strflt/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+   public static void main(String[] args) {
+      String numberAsString = "153.25";
+      float number = Float.parseFloat(numberAsString);
+      System.out.println("The number is: " + number);
+   }
+}
