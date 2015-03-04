26dc1ffe133dbf2eaccb885f12303b3e571ac3cb
malzev <marcos.al.azevedo@gmail.com>
2015-03-04 16:17:51 +0800

    Update and rename intstrzero to intstrzero/Test.java

---
+++ intstrzero/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+   public static void main(String[] args) {
+      int number = 123;
+      String numberAsString = String.format ("%05d", number);
+      System.out.println(numberAsString);
+   }
+}
